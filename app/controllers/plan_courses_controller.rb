99a63ac4b2b6fd11cb3f7e0ebf7279d1a3c78401
Ian Steptoe <steptoeianb@gmail.com>
2020-04-22 15:54:26 -0400

    edits

--- app/controllers/plan_courses_controller.rb
+++ app/controllers/plan_courses_controller.rb
@@ -1,4 +1,5 @@
 class PlanCoursesController < ApplicationController
+    # post /plancourse
     def create
         plancourse = PlanCourse.new
         plancourse.plan_id = Plan.where(user_id: User.where(login: params[:user])[0].id, name: params[:plan])[0].id
@@ -7,14 +8,17 @@ class PlanCoursesController < ApplicationController
         plancourse.year = params[:year]
         plancourse.save!
     end
-
+    # get /plancourse
     def index
         plancourse = PlanCourse.where(course_id: Course.where(designator: params[:course])[0].id, plan_id: Plan.where(user_id: User.where(login: params[:user])[0].id, name: params[:plan])[0].id)[0]
         plancourse.destroy
     end
+
+    # delete /plancourse/3
     def destroy
         @plan_course.destroy
     end
+    
     private
     def set_plan_course
       @plan_course = PlanCourse.find(params[:id])
